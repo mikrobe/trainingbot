90be11aee2f606a575e8672f05b7dcbb8007cd10
Leo Tolstoy <eaudet@jarics.com>
2019-02-21 23:39:16 -0500

    Added activities based classification test.

--- service/src/test/java/com/jarics/trainbot/MLTest.java
+++ service/src/test/java/com/jarics/trainbot/MLTest.java
@@ -1,11 +1,14 @@
 package com.jarics.trainbot;
 
 import com.jarics.trainbot.com.jarics.trainbot.utils.JsonUtil;
+import com.jarics.trainbot.entities.AthleteActivity;
 import com.jarics.trainbot.entities.AthleteFTP;
 import com.jarics.trainbot.entities.AthletesFeatures;
+import com.jarics.trainbot.entities.BotActivityType;
 import com.jarics.trainbot.services.AthleteRepositoryService;
 import com.jarics.trainbot.services.MLClasses;
 import com.jarics.trainbot.services.StravaService;
+import com.jarics.trainbot.services.learning.FeatureExtractor;
 import com.jarics.trainbot.services.learning.WekaMLService;
 import org.junit.After;
 import org.junit.Assert;
@@ -21,6 +24,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -91,5 +96,35 @@ public class MLTest {
         MLClasses mlClasse = wekaMLService.classify(athletesFeatures.gettSB(), athletesFeatures.getcTL(), athletesFeatures.getaTL());
         athleteFTP.setClassification(mlClasse);
         System.out.println(athletesFeatures.toArffData());
+    }
+
+    @Test
+    public void testNormalAthlete() throws Exception {
+        List<AthleteActivity> activities = new ArrayList<>();
+        activities.add(new AthleteActivity(0, BotActivityType.SWIM, 1962, 1962, 1612.9f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.RUN, 2523, 2523, 0.0f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.RUN, 2514, 2443, 4893.6f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.SWIM, 2098, 2098, 1582.4f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.BIKE, 3643, 3612, 34013.4f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.RUN, 2531, 2531, 0.0f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.SWIM, 1938, 1938, 1575.0f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.BIKE, 3899, 3761, 35193.2f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.RUN, 2400, 2400, 4600.0f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.SWIM, 1954, 1954, 1500.0f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.BIKE, 3251, 2786, 19643.7f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.RUN, 2684, 2684, 0.0f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.SWIM, 1586, 1586, 1000.0f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.BIKE, 2001, 2001, 19207.5f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.BIKE, 1354, 1308, 12486.6f, 0.0, 0.0));
+        activities.add(new AthleteActivity(0, BotActivityType.BIKE, 3637, 3604, 33557.7f, 0.0, 0.0));
+        FeatureExtractor wFeatureExtractor = new FeatureExtractor();
+        AthletesFeatures extract = wFeatureExtractor.extract(activities, 101.0, 228.0, 345.0);
+        AthleteFTP athleteFTP = new AthleteFTP();
+        athleteFTP.setUsername("normal guy");
+        extract.setAthlete(athleteFTP);
+        MLClasses mlClasse = wekaMLService.classify(extract.gettSB(), extract.getcTL(), extract.getaTL());
+        athleteFTP.setClassification(mlClasse);
+        Assert.assertEquals(mlClasse, MLClasses.normal);
+        System.out.println(extract.toArffData());
     }
 }
